3f7230b46c5c923a4b203d099943d4cb13cdf24b
Seongsu Cho <whtjdtn310@gmail.com>
2023-01-03 01:35:56 +0900

    feat: 별찍기 - 16, 17

---
+++ src/star/baekjoon10991.java
@@ -0,0 +1,33 @@
+package star;
+
+/*
+ *   문제 : 예제를 보고 규칙을 유추한 뒤에 별을 찍어 보세요.
+ *   입력 : 첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+ *   출력 : 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+ *   예제 :
+ *   1       *
+ *   2       *
+ *          * *
+ *   3       *
+ *          * *
+ *         * * *
+ */
+
+import java.util.Scanner;
+
+public class baekjoon10991 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
