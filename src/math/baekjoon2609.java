c3631be4961912ac6f90796b60e1dbda83c507c2
Seongsu Cho <whtjdtn310@gmail.com>
2023-01-05 01:00:39 +0900

    feat: 최대공약수와 최소공배수 - 1934, 2609, 9613

---
+++ src/math/baekjoon2609.java
@@ -0,0 +1,34 @@
+package math;
+
+/*
+ *   문제 : 두 개의 자연수를 입력받아 최대 공약수와 최소 공배수를 출력하는 프로그램을 작성하시오.
+ *   입력 : 첫째 줄에는 두 개의 자연수가 주어진다. 이 둘은 10,000이하의 자연수이며 사이에 한 칸의 공백이 주어진다.
+ *   출력 : 첫째 줄에는 입력으로 주어진 두 수의 최대공약수를, 둘째 줄에는 입력으로 주어진 두 수의 최소 공배수를 출력한다.
+ *   예제 :
+ *   입력
+ *   24 18
+ *   출력
+ *   6
+ *   72
+ */
+
+import java.util.Scanner;
+
+public class baekjoon2609 {
+    public static int getGCD(int a, int b) {
+        if(b == 0) {
+            return a;
+        } else {
+            return getGCD(b, a % b);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int gcd = getGCD(a,b);
+        int lcm = a * b / gcd;
+        System.out.println(gcd);
+        System.out.println(lcm);
+    }
+}
