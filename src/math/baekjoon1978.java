fb1d3de5942752e0545417fd90afd53e25053407
Seongsu Cho <whtjdtn310@gmail.com>
2023-01-09 23:13:16 +0900

    feat: 소수 구하기 - 1978

---
+++ src/math/baekjoon1978.java
@@ -0,0 +1,29 @@
+package math;
+
+import java.util.Scanner;
+
+public class baekjoon1978 {
+    public static boolean getPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int prime = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (getPrime(sc.nextInt())) prime++;
+        }
+
+        System.out.println(prime);
+    }
+}
