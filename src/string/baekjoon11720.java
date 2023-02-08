861e8255a469c23829b242e03a9bb530df3a22ff
Seongsu Cho <whtjdtn310@gmail.com>
2023-02-08 20:26:38 +0900

    feat: 숫자의 합 - 11720

---
+++ src/string/baekjoon11720.java
@@ -0,0 +1,34 @@
+package string;
+
+/*
+ *   문제 : N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
+ *   입력 : 첫째 줄에 숫자의 개수 N (1 ≤ N ≤ 100)이 주어진다. 둘째 줄에 숫자 N개가 공백없이 주어진다.
+ *   출력 : 입력으로 주어진 숫자 N개의 합을 출력한다.
+ *   예제 :
+ *   입력
+ *   1
+ *   1
+ *   5
+ *   54321
+ *   출력
+ *   1
+ *   15
+ */
+
+import java.util.Scanner;
+
+public class baekjoon11720 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String strNum = sc.next();
+        char[] cNum = strNum.toCharArray();
+        int sum = 0;
+
+        for (int i = 0; i < cNum.length; i++) {
+            sum += cNum[i] - '0';
+        }
+
+        System.out.print(sum);
+    }
+}
